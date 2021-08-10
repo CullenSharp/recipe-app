b6be33b2852329011610a5e8379765d0893a663b
Cullen Sharp <cullen.sharp100@gmail.com>
2021-08-09 18:45:30 -0700

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Hello, World 🌎</Text>
     </View>
   );
 }
@@ -15,7 +14,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+
+  heading: {
+    fontSize: 40,
+  }
 });
